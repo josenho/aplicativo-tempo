bb355d2fc5d9bba4fd1b2c09899a9b2ae9808905
Jose Mateus <josemateus@alu.ufc.br>
2025-02-03 11:46:41 -0300

    primeiro commit
    Conhecendo ambiente e testando Story Board

---
+++ Aplicativo do Tempo/Aplicativo do Tempo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aplicativo do Tempo
+//
+//  Created by aluno on 03/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
